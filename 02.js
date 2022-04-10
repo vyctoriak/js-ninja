32bc184a3bbb775fa27c5acddaf89488eaa069fd
vyctoria karina <vyctoriak@gmail.com>
2022-04-10 22:02:08 +0200

    challenge 02

---
+++ 02.js
@@ -0,0 +1,107 @@
+// Desafio da semana #2
+
+// Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções!
+
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+
+function soma(argument1, argument2){
+  return argument1 + argument2;
+}
+
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+
+var somando = soma(5, 5) + 10
+
+// Qual o valor atualizado dessa variável?
+
+console.log("O valor total é de", somando);
+// 20
+
+// Declare uma nova variável, sem valor.
+
+var semValor;
+
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é VALOR.
+Onde VALOR é o novo valor da variável.
+*/
+
+function adicionandoValor(valorAdicionado) {
+  semValor = valorAdicionado
+  return(
+    'O valor da variável agora é ' + semValor
+  )
+}
+// Invoque a função criada acima.
+
+adicionandoValor(20)
+
+// Qual o retorno da função? (Use comentários de bloco).
+
+// 20
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+
+function conditionalFunction(a, b, c) {
+  if(a == undefined || b == undefined || c == undefined) {
+    return 'Preencha todos os valores corretamente!';
+  }
+
+  return (a * b * c) + 2;
+}
+
+// Invoque a função criada acima, passando só dois números como argumento.
+
+console.log(conditionalFunction(1, 2));
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+
+// Preencha todos os valores corretamente!
+
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+
+console.log(conditionalFunction(2, 4, 6));
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+
+// 50
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos.
+2. Se somente um argumento for passado, retorne o valor do argumento.
+3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+*/
+
+function secondConditionFunction(a, b, c) {
+  if(a !== undefined && b == undefined && c == undefined) {
+    return a;
+  } else if(a !== undefined && b !== undefined && c == undefined) {
+    return a + b;
+  } else if(a !== undefined && b !== undefined && c !== undefined) {
+    return (a + b) / 2;
+  } else if(a !== undefined && b !== undefined && c !== undefined) {
+    return false;
+  } else {
+    return null;
+  }
+}
+
+// Invoque a função acima utilizando todas as possibilidades 
+// (com nenhum argumento, com um, com dois e com três.) 
+//Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+
+console.log(secondConditionFunction()) // nenhum argumento == null
+console.log(secondConditionFunction(1)) // um argumento == 1
+console.log(secondConditionFunction(1, 2)) // dois argumentos == 3 
+console.log(secondConditionFunction(1, 2, 3)) // três argumentos == 1.5
